7e8f4c481d5a1c1fb84227a70ab61fe37b918d3a
Jhiguitam <jhiguitam@unal.edu.co>
2022-05-17 18:46:39 -0500

    Nota enum ingredientes

--- SoftwareRestaurante/restaurante/Proveedor.java
+++ SoftwareRestaurante/restaurante/Proveedor.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 public class Proveedor {	 
     ingredientes lista_arts;
-
+    // Debido a la estructura de enum no es ´ñposible crear un constructor que contenga todos los elementos del enum e instacnciarlas
+    // Hay que replantear clase Ingredientes 
     public Proveedor(ingredientes arts){
         this.lista_arts=arts;
     }
