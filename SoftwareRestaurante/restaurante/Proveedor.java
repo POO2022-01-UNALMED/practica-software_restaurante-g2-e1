ce7b1432c85113b64d9856eab8a1c76592c146f9
Jhiguitam <jhiguitam@unal.edu.co>
2022-05-16 21:01:06 -0500

    Inicio proveddor

--- SoftwareRestaurante/restaurante/Proveedor.java
+++ SoftwareRestaurante/restaurante/Proveedor.java
@@ -1,7 +1,23 @@
 package restaurante;
 import java.util.ArrayList; 
 import java.util.Collections;
-public class Proveedor {
-	
+public class Proveedor {	 
+    ingredientes lista_arts;
+
+    public Proveedor(ingredientes arts){
+        this.lista_arts=arts;
+    }
+
+    public void setLArticulo(){
+        
+    }
+
+    public ingredientes getLArticulos(){
+        
+
+        
+    }
+    
+    
 
 }
