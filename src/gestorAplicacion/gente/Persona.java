f8d68b5841855d1107e6eb4d9804cc78a7fe01cd
juvilla <juvillamizarg@unal.edu.co>
2022-06-02 18:03:32 -0500

    asdafaw
    descripcion detallada

--- src/gestorAplicacion/gente/Persona.java
+++ src/gestorAplicacion/gente/Persona.java
@@ -1,18 +1,23 @@
 package gestorAplicacion.gente;
 import java.io.Serializable;
 
+//persona es una clase abstracta que hereda caracteristicas y metodos utiles para las clases
+//Gerente, Cliente y Empleado
 public abstract class Persona implements Serializable{
-private int cedula;
-private String nombre;
-private int telefono;
+private int cedula; //numero de cedula de la persona
+private String nombre; //nombre de la persona
+private int telefono; //telefono de la persona
 
 //constructor
-public abstract String perfil();
+public abstract String perfil(); //metodo abstracto que es implementado por las clases Cliente, Empleado y Persona
 
-public String informacion() {
+
+
+public String informacion() { //metodo que devuelve un string con la informacion de la persona
 	return "cedula: "+cedula+" nombre: "+nombre+" telefono: "+telefono;
 }
 
+//constructor para persona
 public Persona(int cedula,String nombre,int telefono) {
 	this.cedula=cedula;
 	this.nombre=nombre;
@@ -21,21 +26,26 @@ public Persona(int cedula,String nombre,int telefono) {
 
 //metodos get y set
 
+//metodo que devuelve la cedula de la persona
 public int getCedula() {
 	return cedula;
 }
 
+//metodo que devuelve el nombre de la persona
 public String getNombre() {
 	return nombre;
 }
 
+//metodo que establece el nombre de la persona
 public void setNombre(String nombre) {
 	this.nombre=nombre;
 }
+//metodo que devuelve el telefono de la persona
 public int getTelefono() {
 	return telefono;
 }
 
+//metodo que establece el telefono de la persona
 public void setTelefono(int telefono) {
 	this.telefono=telefono;
 }
