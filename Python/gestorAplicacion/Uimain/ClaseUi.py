3e0fd17ce5d96c127ca9f548f9189ac2d24fa62c
juvilla <juvillamizarg@unal.edu.co>
2022-06-21 11:25:22 -0500

    Update ClaseUi.py

--- Python/gestorAplicacion/Uimain/ClaseUi.py
+++ Python/gestorAplicacion/Uimain/ClaseUi.py
@@ -1,1 +1,13 @@
-print "hola"
+#esta es la clase en la que va a ir la interfaz y demas vainas importantes
+import tkinter as tk
+
+inicio = tk.Tk()
+inicio.title("inicio")
+inicio.geometry("500x400")
+
+
+P3=tk.Label(inicio,text="bienvenido") #da un mensaje de bienvenida
+P3.pack(anchor="nw")
+
+inicio.mainloop()
+
