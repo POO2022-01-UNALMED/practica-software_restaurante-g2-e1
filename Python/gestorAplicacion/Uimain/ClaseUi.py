18c7677085c5504d402b406ef1fba65ae6ed9194
juvilla <juvillamizarg@unal.edu.co>
2022-06-21 11:57:33 -0500

    Update ClaseUi.py

--- Python/gestorAplicacion/Uimain/ClaseUi.py
+++ Python/gestorAplicacion/Uimain/ClaseUi.py
@@ -4,10 +4,15 @@ import tkinter as tk
 inicio = tk.Tk()
 inicio.title("inicio")
 inicio.geometry("500x400")
+inicio.configure(bg="red")
 
-
-P3=tk.Label(inicio,text="bienvenido") #da un mensaje de bienvenida
+P3=tk.Label(inicio,text="bienvenido al sofware para un restaurante") #da un mensaje de bienvenida
 P3.pack(anchor="nw")
+P3.config(fg="white",bg="red",font=("Arial",20))
 
 inicio.mainloop()
 
+#hoja de vida villamizar:
+#Nombres: Juan David
+#Apellidos: Villamizar Gelves
+#edad: 21 años
