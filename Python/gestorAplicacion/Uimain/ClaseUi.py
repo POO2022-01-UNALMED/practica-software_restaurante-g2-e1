2d1a65fea3bbdbcf5ab9db41c9b77db2a7a8e024
juvilla <juvillamizarg@unal.edu.co>
2022-06-24 15:05:03 -0500

    v

--- Python/gestorAplicacion/Uimain/ClaseUi.py
+++ Python/gestorAplicacion/Uimain/ClaseUi.py
@@ -2,13 +2,16 @@ import tkinter as tk
 import os
 import pathlib
 from tkinter import messagebox
+from tkinter import ttk
 
 path = os.path.join(pathlib.Path(__file__).parent.absolute())
+
+#ventana de inicio, es la primera ventana que se muestra al iniciar sesion
 
 class ventana_inicio(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("ventana_1")
+        self.title("ventana_inicio")
         self.geometry("500x400")
         self.configure(bg="red")
 
@@ -220,7 +223,7 @@ class ventana_inicio(tk.Tk):
 
         #boton para ir a la siguiente ventana
 
-        self.boton_i_sesion = tk.Button(self.P4,text="iniciar sesion")
+        self.boton_i_sesion = tk.Button(self.P4,text="iniciar sesion",command=self.ventana_principal)
 
         self.boton_i_sesion.grid(row=1,column=0)
 
@@ -235,6 +238,101 @@ class ventana_inicio(tk.Tk):
         self.separador2.grid(row=1,column=1)
 
         self.mainloop()
+    
+    def ventana_principal(self):
+        self.destroy()
+        ventana_principal()
 
+#ventana principal, donde se ejecuta el programa
+
+class ventana_principal(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("ventana_principal")
+        self.geometry("500x400")
+        self.configure(bg="white")
+
+        #este frame(frame_0) corresponde al de la parte superior
+
+        self.frame_0=tk.Frame(self,bg="red",height=80)
+        self.frame_0.pack(side="top",fill="x")
+
+        #una etiqueta que contiene al titulo del nombre del software
+
+        self.titulo=tk.Label(self.frame_0,text=" software para un restaurante ")
+        self.titulo.config(fg="white",bg="red",font=("italic",30,"italic"))
+        self.titulo.pack(anchor="c")
+
+        #este frame(frame_1) corresponde a la zona de menus
+
+        self.frame_1=tk.Frame(self,bg="gray",height=80)
+        self.frame_1.pack(side="top",fill="x")
+
+        #se muestra una ventana de dialogo con la descripcion del software
+        def descripcion_software(): 
+            messagebox.showinfo("Descripcion del sistema","Este software se encarga de la gestion de un restaurante a nivel general, es decir, con el software para un restaurante se podran administrar diferentes aspectos como lo son ventas, nomina e inventario")
+    
+        #se muestra una ventana de dialogo con los nombres de los autores del software
+        def autores_software(): 
+            messagebox.showinfo("Autores del software","Autor 1:Juan David Villamizar Gelves\nAutor 2:Jorge Andres Higuita Monsalve\nAutor 3:Juan Sebastian Echeverry Zapata")
+
+        #esta funcion ejecuta las opciones del combobox de Archivo
+        def opciones_Archivo(event):
+            if self.Archivo.get()=="Aplicacion":
+                self.Archivo.set("Archivo")
+                descripcion_software()
+                
+            if self.Archivo.get()=="Salir":
+                self.Archivo.set("Archivo")
+                self.ventana_inicio()
+            
+        #esta funcion ejecuta las opciones del combobox de opciones_p_y_C(todavia faltan por definir bien las funciones)
+        def opciones_p_y_c(event):
+            if self.p_y_c.get()=="realizar pedido":
+                self.p_y_c.set("Procesos y consultas")
+                pass
+            if self.p_y_c.get()=="cancelar pedido":
+                self.p_y_c.set("Procesos y consultas")
+                pass
+        
+        #esta funcion ejecuta las opciones de ayuda
+        def opciones_ayuda(event):
+            if self.Ayuda.get()=="acerca de":
+                self.Ayuda.set("Ayuda")
+                autores_software()
+
+
+
+        #Archivo
+        valorDefecto_archivo = tk.StringVar(value='Archivo')
+        self.Archivo = tk.ttk.Combobox(self.frame_1,values=["Aplicacion","Salir"],textvariable=valorDefecto_archivo,state="readonly")
+        self.Archivo.grid(row=0,column=0)
+        self.Archivo.bind("<<ComboboxSelected>>",opciones_Archivo)
+
+
+        #Procesos y consultas
+        valorDefecto_p_y_c = tk.StringVar(value='Procesos y consultas')
+        self.p_y_c = tk.ttk.Combobox(self.frame_1,values=["realizar pedido","cancelar pedido"],textvariable=valorDefecto_p_y_c,state="readonly")
+        self.p_y_c.grid(row=0,column=1)
+        self.p_y_c.bind("<<ComboboxSelected>>",opciones_p_y_c)
+
+        #Ayuda
+        valorDefecto_ayuda = tk.StringVar(value='Ayuda')
+        self.Ayuda = tk.ttk.Combobox(self.frame_1,values=["acerca de"],textvariable=valorDefecto_ayuda,state="readonly")
+        self.Ayuda.grid(row=0,column=2)
+        self.Ayuda.bind("<<ComboboxSelected>>",opciones_ayuda)
+
+
+        #este frame va en el fondo, su funcion es meramente estetica
+        self.frame_fondo=tk.Frame(self,bg="red",height=80)
+        self.frame_fondo.pack(side="bottom",fill="x")
+
+        self.mainloop()
+
+    #vuelve a la ventana de inicio    
+    def ventana_inicio(self):
+        self.destroy()
+        ventana_inicio()
 
 ventana_inicio()
+#ventana_principal()
