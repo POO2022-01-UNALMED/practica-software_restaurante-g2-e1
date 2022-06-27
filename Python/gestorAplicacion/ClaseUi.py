d398d9e8db78a9a7b0489a81f89c3a997bd4bc02
Jhiguitam <jhiguitam@unal.edu.co>
2022-06-27 02:11:28 -0500

    Cambios y correciones

--- Python/gestorAplicacion/ClaseUi.py
+++ Python/gestorAplicacion/ClaseUi.py
@@ -1,3 +1,17 @@
 import ventana_inicio 
+import os
+from gente.empleado import Empleado
+from baseDatos.Serializador import Serializador
+from baseDatos.Deserializador import Deserializador
 
+#Comprobanmos si existen objetos creados anteriormente
+if os.stat("Python/gestorAplicacion/baseDatos/temp/Empleado.txt").st_size == 0:
+    p1 = Empleado(1, "Juan", 123)
+    p2 = Empleado (2, "Villa", 456)
+else:
+    # Se cargan los objetos guardados
+    Deserializador.deserializar("Empleado")
+    print("Deserializaondo WUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU")
 ventana_inicio.Ventana_inicio()
+Serializador.serializar("Empleado")
+
--- Python/gestorAplicacion/baseDatos/Deserializador.py
+++ Python/gestorAplicacion/baseDatos/Deserializador.py
@@ -1,32 +1,41 @@
 import pickle
 
-from gestorAplicacion.gente.cliente import Cliente
-from gestorAplicacion.gente.empleado import Empleado
-from gestorAplicacion.gente.gerente import Gerente
-from gestorAplicacion.restaurante.ingredientes import Ingredientes
+from gente.cliente import Cliente
+from gente.empleado import Empleado
+from gente.gerente import Gerente
+from restaurante.ingredientes import Ingredientes
 
-def deserializar(cls, tipo):
-    if tipo == "Cliente":
-        pickfile = open("Python/baseDatos/temp/Cliente.txt", "rb")
-        Cliente._lista_socio = pickle.load(pickfile)
-        pickfile.close
+class Deserializador:
+    @classmethod
+    def deserializar(cls, tipo):
+        if tipo == "Cliente":
+            pickfile = open("Python/gestorAplicacion/baseDatos/temp/Cliente.txt", "rb")
+            Cliente._lista_socio = pickle.load(pickfile)
+            pickfile.close
 
-    elif tipo == "Empleado":
-        pickfile = open("Python/baseDatos/temp/Empleado.txt", "rb")
-        Empleado._lista_empleado = pickle.load(pickfile)
-        pickfile.close
+        elif tipo == "Empleado":
+            pickfile = open( "Python/gestorAplicacion/baseDatos/temp/Empleado.txt", "rb")
+            Empleado._lista_empleado = pickle.load(pickfile)
+            pickfile.close
 
-    elif tipo == "Gerente":
-        pickfile = open("Python/baseDatos/temp/Gerente.txt", "rb")
-        Gerente._list_gerente = pickle.load(pickfile)
-        pickfile.close
+        elif tipo == "Gerente":
+            pickfile = open("Python/gestorAplicacion/baseDatos/temp/Gerente.txt", "rb")
+            Gerente._list_gerente = pickle.load(pickfile)
+            pickfile.close
 
-    elif tipo == "ingredientes":
-        pickfile = open("Python/baseDatos/temp/ingredientes.txt", "rb")
-        Ingredientes._listaIngredientes = pickle.load(pickfile)
-        pickfile.close
+        elif tipo == "ingredientes":
+            pickfile = open("Python/gestorAplicacion/baseDatos/temp/ingredientes.txt", "rb")
+            Ingredientes._listaIngredientes = pickle.load(pickfile)
+            pickfile.close
 
-    elif tipo == "NumEmple":
-        pickfile = open("Python/baseDatos/temp/NumEmple.txt", "rb")
-        Empleado._numero_empleados = pickle.load(pickfile)
-        pickfile.close
+        elif tipo == "NumEmple":
+            pickfile = open("Python/gestorAplicacion/baseDatos/temp/NumEmple.txt", "rb")
+            Empleado._numero_empleados = pickle.load(pickfile)
+            pickfile.close
+
+    def DeserializarTodo():
+        Deserializador.deserializar("Cliente")
+        Deserializador.deserializar("Empleado")
+        Deserializador.deserializar("Gerente")
+        Deserializador.deserializar("ingredientes")
+        Deserializador.deserializar("NumEmple")
