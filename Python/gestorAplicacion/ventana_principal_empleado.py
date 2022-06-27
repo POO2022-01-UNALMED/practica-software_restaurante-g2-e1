b9fb599159a5717e593fa376cb44fb364db4baf3
juvilla <juvillamizarg@unal.edu.co>
2022-06-27 10:33:37 -0500

    Update ventana_principal_empleado.py

--- Python/gestorAplicacion/ventana_principal_empleado.py
+++ Python/gestorAplicacion/ventana_principal_empleado.py
@@ -7,6 +7,11 @@ import ventana_inicio
 import iniciar_sesion
 import fieldframe
 
+from gente.empleado import Empleado
+from restaurante.ingredientes import Ingredientes
+from restaurante.orden import Orden
+from gente.cliente import Cliente
+
 path = os.path.join(pathlib.Path(__file__).parent.absolute())
 
 #ventana principal, donde se ejecuta el programa
@@ -61,45 +66,85 @@ class Ventana_principal_empleado(tk.Tk):
         
         #esta funcion se ejecuta al seleccionar la opcion agragar socio del combobox de p y c
         def agregar_socio():
-            global a
+
+            self.titulo_tomar_orden.place_forget()
+
+            self.descripcion_tomar_orden.place_forget()
 
             self.boton_anadir_platillo.place_forget()
 
             self.boton_retirar_platillo.place_forget()
 
-            self.boton_revisar_pedido.place_forget()
-
             self.boton_terminar_orden.place_forget()
 
             self.boton_cancelar_orden.place_forget()
 
-            a=fieldframe.FieldFrame(self.frame_2,"datos",["nombre","cedula"],"valor")
+            self.titulo_anadir_socio.place(x=690,y=10,width=100,height=40)
 
-            a.place(x=520,y=100,width=500,height=100)
+            self.descripcion_anadir_socio.place(x=400,y=60,width=700,height=40)
+
+            self.a.place(x=520,y=110,width=500,height=100)
         
         #esta funcion se ejecuta al seleccionar la opcion tomar orden del combobox de p y c
         def tomar_orden():
-            global a
 
-            self.boton_anadir_platillo.place(x=640,y=50,width=300,height=50)
+            orden=Orden()
 
-            self.boton_retirar_platillo.place(x=640,y=110,width=300,height=50)
+            self.titulo_anadir_socio.place_forget()
 
-            self.boton_revisar_pedido.place(x=640,y=170,width=300,height=50)
+            self.descripcion_anadir_socio.place_forget()
 
-            self.boton_terminar_orden.place(x=640,y=230,width=300,height=50)
+            self.titulo_tomar_orden.place(x=730,y=10,width=100,height=40)
 
-            self.boton_cancelar_orden.place(x=640,y=290,width=300,height=50)
+            self.descripcion_tomar_orden.place(x=540,y=70,width=500,height=40)
 
-            a.place_forget()
+            self.boton_anadir_platillo.place(x=640,y=120,width=300,height=50)
+
+            self.boton_retirar_platillo.place(x=640,y=180,width=300,height=50)
+
+            self.boton_terminar_orden.place(x=640,y=240,width=300,height=50)
+
+            self.boton_cancelar_orden.place(x=640,y=300,width=300,height=50)
+
+            self.a.place_forget()
+        
+        #cosas de anadir socio
+
+        self.titulo_anadir_socio = tk.Label(self.frame_2,text=" anadir socio ")
+
+        self.titulo_anadir_socio.place(x=10,y=10,width=100,height=40)
+
+        self.titulo_anadir_socio.place_forget()
+
+        self.descripcion_anadir_socio = tk.Label(self.frame_2,text=" esta funcion se encarga de registrar a los clientes a una lista de socios, los cuales tienen acceso a descuentos ")
+
+        self.descripcion_anadir_socio.place(x=10,y=10,width=100,height=40)
+
+        self.descripcion_anadir_socio.place_forget()
+
+        self.a=fieldframe.FieldFrame(self.frame_2,"datos",["nombre","cedula"],"valor")
+
+        self.a.place(x=520,y=100,width=500,height=100)
+
+        self.a.place_forget()
         
         #botones de tomar orden
+
+        self.titulo_tomar_orden = tk.Label(self.frame_2,text=" tomar orden ")
+
+        self.titulo_tomar_orden.place(x=10,y=10,width=100,height=40)
+
+        self.titulo_tomar_orden.place_forget()
+
+        self.descripcion_tomar_orden = tk.Label(self.frame_2,text=" esta funcion se encarga de la gestion de los pedidos a nivel general")
+
+        self.descripcion_tomar_orden.place(x=10,y=10,width=100,height=40)
+
+        self.descripcion_tomar_orden.place_forget()
 
         self.boton_anadir_platillo = tk.Button(self.frame_2,text="anadir platillo")
 
         self.boton_retirar_platillo = tk.Button(self.frame_2,text="retirar platillo")
-
-        self.boton_revisar_pedido = tk.Button(self.frame_2,text="revisar pedido")
 
         self.boton_terminar_orden = tk.Button(self.frame_2,text="terminar orden")
 
@@ -111,8 +156,6 @@ class Ventana_principal_empleado(tk.Tk):
 
         self.boton_retirar_platillo.place(x=10,y=20,width=100,height=40)
 
-        self.boton_revisar_pedido.place(x=10,y=30,width=100,height=40)
-
         self.boton_terminar_orden.place(x=10,y=40,width=100,height=40)
 
         self.boton_cancelar_orden.place(x=10,y=50,width=100,height=40)
@@ -123,11 +166,20 @@ class Ventana_principal_empleado(tk.Tk):
 
         self.boton_retirar_platillo.place_forget()
 
-        self.boton_revisar_pedido.place_forget()
-
         self.boton_terminar_orden.place_forget()
 
         self.boton_cancelar_orden.place_forget()
+
+        ###########################################################################
+
+        #funcionalidades dentro de tomar orden
+
+        #def añadiendo_platillo():
+        #    self.ventana_platillo()
+
+        #def retirando_platillo():
+
+        #def terminar_orden():       
 
         ############################################################################
                
@@ -166,10 +218,11 @@ class Ventana_principal_empleado(tk.Tk):
         self.Ayuda = tk.ttk.Combobox(self.frame_1,values=["acerca de"],textvariable=valorDefecto_ayuda,state="readonly")
         self.Ayuda.grid(row=0,column=2)
         self.Ayuda.bind("<<ComboboxSelected>>",opciones_ayuda)
-
         self.mainloop()
 
     #vuelve a la ventana de inicio   
     def ventana_inicio(self):
         self.destroy()
         ventana_inicio.Ventana_inicio()
+
+Ventana_principal_empleado()
